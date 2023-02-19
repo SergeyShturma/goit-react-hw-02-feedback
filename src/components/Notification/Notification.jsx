e0c344359364f53430705fe0c19ad897a512d275
SergeyShturma <sergeyshturma@gmail.com>
2023-02-19 14:11:14 +0200

    fun

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,17 @@ import PropTypes from 'prop-types';
 import css from './Notification.module.css';
 
 export default function Notification({ message }) {
-  return <p className={css.message}>{message}</p>;
+  return (
+    <>
+      <p className={css.message}>{message}</p>
+      <iframe
+        className={css.gif}
+        src="https://giphy.com/embed/3o7aTskHEUdgCQAXde"
+        alt="gif"
+        allowFullScreen
+      />
+    </>
+  );
 }
 
 Notification.propTypes = {
